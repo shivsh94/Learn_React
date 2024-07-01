a2ec0a876394d393507c31dedb097296c0cb9565
shivsh94 <shivamsharma23jan@gmail.com>
2024-07-01 16:42:59 +0530

    Counter_hooks_project_01

---
+++ 02_counter/src/App.jsx
@@ -0,0 +1,40 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  let [counter, setCounter] = useState(15);
+   
+  // let Counter = 6
+
+  const addValue = () => {
+    if(counter <20){
+       //  Counter += 1
+        setCounter(counter + 1);
+    }
+   
+  }
+  const removeValue = () => {
+    if(counter >0){
+      // Counter -= 1
+    setCounter(counter - 1);
+    }
+    }
+
+  return ( 
+    <>
+      <h1>hello react</h1>
+      <h2>Counter value: {counter}</h2>
+
+      <button
+      onClick={addValue}>
+        Add value</button>
+      <br />
+      <button
+      onClick={removeValue}>remove value</button>
+    </>
+  )
+}
+
+export default App
